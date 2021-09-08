e55454bfcb245078965b36dc5c11890676e01847
Akshara Johari <akshara.johari.14@gmail.com>
2021-09-08 23:08:48 +0530

    Practice

--- Question2.cpp
+++ Question2.cpp
@@ -1,68 +1,90 @@
+// Name: Akshara Johari
+// Roll number: 201378
+
 #include <iostream>
 using namespace std;
 
-class Node
+class bank
 {
-    public :    
-        int data;
-        Node* next;
+
+public:
+    char name[30];
+    int Account_No, bal;
+    char type[15];
+
+    void enter()
+    {
+
+        cout << "Enter Your Account Name : ";
+        cin >> name;
+        cout << "Enter Your Account No. : ";
+        cin >> Account_No;
+        cout << "Enter Your Account type : ";
+        cin >> type;
+        cout << "Enter Your Account Balance : ";
+        cin >> bal;
+    }
+
+    void display()
+    {
+        cout << "Account Name : " << name << " ";
+        cout << "Account No. : " << Account_No << " ";
+        cout << "Account Type : " << type << " ";
+        cout << "Available Balance : " << bal << " ";
+    }
+
+    int deposit()
+    {
+        return bal;
+    }
 };
-
-void display(Node* head)
-{
-    Node* temp = head;
-    while(temp!=NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-}
-
-struct Node * insertAtIndex(struct Node *head, int data, int index){
-    struct Node * ptr = (struct Node *) malloc(sizeof(struct Node));
-    struct Node * p = head;
-    int i = 0;
-
-    while (i!=index-1)
-    {
-        p = p->next;
-        i++;
-    }
-    ptr->data = data;
-    ptr->next = p->next;
-    p->next = ptr;
-    return head;
-}
 
 int main()
 {
-    int n , index, data;
-    Node* head = NULL;
-    Node* prev = NULL;
-    cout << "Enter size : ";
-    cin >> n;
-    cout << "Enter values : ";
-    for(int i=0;i<n;i++)
-    {
-        Node* n = new Node();
-        cin >> n->data;
-        n->next = NULL;
 
-        if(head==NULL)
-        {
-            head = n;
-            prev = n;
-        }            
-        else
-        {
-            prev->next = n;
-            prev = prev->next;
-        }
+    int n, index, dep_amount, new_balance, withdraw_amount, left_balance;
+
+    cout << "No. of accounts you wanna create : " << endl;
+    cin >> n;
+    bank cust[n];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << "Enter the details of Account " << i + 1 << " : \n";
+        cust[i].enter();
     }
-    cout << "Enter index of insertion : ";
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\n Account " << i + 1 << " Entered details : \n";
+        cust[i].display();
+    }
+
+    // Deposit
+    cout << "\n\n Enter account index to Deposit money : ";
     cin >> index;
-    cout << "Enter data to be inserted : ";
-    cin >> data;
-    insertAtIndex(head, data, index);
-    display(head);    
+    cout << "Deposit amount : ";
+    cin >> dep_amount;
+    new_balance = cust[index - 1].deposit() + dep_amount;
+    cust[index - 1].bal = new_balance;
+    cout << "\nNew Balance in account " << index << " : " << cust[index - 1].bal;
+
+    // Withdraw
+    cout << "\n\n Enter account index to Withdraw money : ";
+    cin >> index;
+    cout << "Withdraw amount : ";
+    cin >> withdraw_amount;
+    if (cust[index - 1].deposit() > withdraw_amount)
+    {
+        left_balance = cust[index - 1].deposit() - withdraw_amount;
+        cust[index - 1].bal = left_balance;
+        cout << "\nLeft Balance in account " << index << " : " << cust[index - 1].bal;
+    }
+    else
+    {
+        cout << "Error";
+    }
+    return 0;
 }

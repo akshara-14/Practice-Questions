020c3b2438202412699dbf56c1959f4766ec6143
Akshara Johari <akshara.johari.14@gmail.com>
2022-06-19 22:32:11 +0530

    correct formatting

--- BIGARRAY.c
+++ BIGARRAY.c
@@ -10,7 +10,7 @@ int main()
         scanf("%ld %ld", &n, &s);
         long long int sum = (n * (n + 1)) / 2;
 
-        if (sum-s>=1 && sum-s<=n)
+        if (sum - s >= 1 && sum - s <= n)
         {
             printf("%ld\n", sum - s);
         }

ade089fd28508bc1960c9949c111768f3dd4676a
Akshara Johari <akshara.johari.14@gmail.com>
2022-06-20 13:37:46 +0530

    formatting

--- BUYPLSE.c
+++ BUYPLSE.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h>
 
-int main(void) {
-	int a,b,x,y,c,d;
-	scanf("%d %d %d %d", &a,&b,&x,&y);
-	c = a*x;
-	d = b*y;
-	printf("%d",c+d);
+int main(void)
+{
+	int a, b, x, y, c, d;
+	scanf("%d %d %d %d", &a, &b, &x, &y);
+	c = a * x;
+	d = b * y;
+	printf("%d", c + d);
 	return 0;
 }
-

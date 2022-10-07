44b1b55b4c4352a73a7e0097ad168c14cf4833bb
Akshara Johari <akshara.johari.14@gmail.com>
2022-10-07 09:56:12 +0530

    check an element in the array

---
+++ Array1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    int arr[m];
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (arr[i] == n)
+        {
+            cout << "yes\n";
+            goto no;
+        }
+        
+    }
+    cout << "no";
+    no:
+    return 0;
+}

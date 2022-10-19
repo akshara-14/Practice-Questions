f74cc0ac7b4c1f399972f97e9422b317489b5b62
Akshara Johari <akshara.johari.14@gmail.com>
2022-10-20 00:47:10 +0530

    codechef

--- CYCLICQD.c
+++ CYCLICQD.c
@@ -4,19 +4,23 @@
 
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 	int t;
-	scanf("%d",&t);
-	while(t>0){
-	    int a,b,c,d;
-	    scanf("%d %d %d %d", &a, &b, &c, &d);
-	    if(a + c == 180 && b + d == 180){
-	        printf("YES\n");
-	    }
-	    else{
-	        printf("NO\n");
-	    }
-	    t--;
+	scanf("%d", &t);
+	while (t > 0)
+	{
+		int a, b, c, d;
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+		if (a + c == 180 && b + d == 180)
+		{
+			printf("YES\n");
+		}
+		else
+		{
+			printf("NO\n");
+		}
+		t--;
 	}
 	return 0;
 }

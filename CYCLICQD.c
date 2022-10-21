84bc8a5fbbb86bd8f74d4d0363d1d4a809844664
Akshara Johari <akshara.johari.14@gmail.com>
2022-10-21 05:58:31 +0530

    codechef

--- CYCLICQD.c
+++ CYCLICQD.c
@@ -1,7 +1,3 @@
-// You are given the sizes of angles of a simple quadrilateral (in degrees) A, B, C and D, in some order along its perimeter. Determine whether the quadrilateral is cyclic.
-
-// Note: A quadrilateral is cyclic if and only if the sum of opposite angles is 180∘.
-
 #include <stdio.h>
 
 int main(void)

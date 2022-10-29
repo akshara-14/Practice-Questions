47656e9db434f96404cdafcee09524532eadc9c6
Akshara Johari <akshara.johari.14@gmail.com>
2022-10-29 18:59:36 +0530

    pointer

--- PointerArithmetic.c
+++ PointerArithmetic.c
@@ -6,9 +6,9 @@ int main()
     char b = 'c';
     char *ptrb = &b;
     printf("%d\n", ptra);
-    printf("%d\n", ptra + 1); //size of int will be added ,i.e.,(1*4)
+    printf("%d\n", ptra + 1); // size of int will be added ,i.e.,(1*4)
     printf("%d\n", ptrb);
-    printf("%d\n", ptrb + 2); //size of char will be added ,i.e.,(2*1)
+    printf("%d\n", ptrb + 2); // size of char will be added ,i.e.,(2*1)
     ptra++;
     printf("%d\n", ptra);
     ptrb++;

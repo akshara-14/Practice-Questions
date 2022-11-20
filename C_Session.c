dd44960a8273056103f64a0f71c995e7786b6583
Akshara Johari <akshara.johari.14@gmail.com>
2022-11-20 14:43:34 +0530

    C session IEEE

--- C_Session.c
+++ C_Session.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     // int i = 11;
@@ -8,14 +8,12 @@ int main()
     //     printf("%d\n", i);
     //     i++;
     // }
-    
 
     // do{
     //     printf("%d\n", i);
     //     i++;
 
     // }while(i < 11);
-
 
     // int i;
     // for( i = 1; i < 11; ){
@@ -24,26 +22,26 @@ int main()
     //     i++;
     // }
 
-     
     //  1
     //  1 2
     //  1 2 3
     //  1 2 3 4
     //  1 2 3 4 5
-    
-    int i, j, n;     // i -> rows, j -> columns
+
+    int i, j, n; // i -> rows, j -> columns
     printf("Enter the number of rows: ");
     scanf("%d", &n);
 
-    for (int i = 1; i <= n ; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= i; j++){
+        for (int j = 1; j <= i; j++)
+        {
             printf("* ");
         }
-        
+
         printf("\n");
     }
-    
+
     return 0;
 }
 

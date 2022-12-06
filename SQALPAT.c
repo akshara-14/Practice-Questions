9914422e1ff67221e94832b601614807a1838403
Akshara Johari <akshara.johari.14@gmail.com>
2022-12-06 14:14:41 +0530

    codechef

--- SQALPAT.c
+++ SQALPAT.c
@@ -1,12 +1,3 @@
-// You're given a number N. Print the first N lines of the below-given pattern.
-
-// 1 2 3 4 5
-// 10 9 8 7 6
-// 11 12 13 14 15
-// 20 19 18 17 16
-// 21 22 23 24 25
-// 30 29 28 27 26
-
 #include <stdio.h>
 
 int main()

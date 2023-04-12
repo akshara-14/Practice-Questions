67e1bc921f2d8264a89bec6acc0ee6148cf531f2
Akshara Johari <akshara.johari.14@gmail.com>
2023-04-12 09:02:17 +0530

    CN Lab

--- Go-Back-N.cpp
+++ Go-Back-N.cpp
@@ -1,94 +1,37 @@
-#include <iostream>
-#include <cstring>
-#include <cstdio>
-using namespace std;
-
-const int MAX_PACKET_SIZE = 1024;
-const int MAX_SEQUENCE_NUMBER = 7;
-const int WINDOW_SIZE = 4;
-
-struct packet
-{
-    int sequence_number;
-    char data[MAX_PACKET_SIZE];
-};
-
-int base = 0;
-int next_sequence_number = 0;
-packet window[WINDOW_SIZE];
-
-void sender()
-{
-    packet p;
-    while (true)
-    {
-        // Read data from input
-        cin.getline(p.data, MAX_PACKET_SIZE);
-        int len = strlen(p.data);
-        if (len == 0)
-            break;
-
-        // Fill packet fields
-        p.sequence_number = next_sequence_number;
-        next_sequence_number = (next_sequence_number + 1) % (MAX_SEQUENCE_NUMBER + 1);
-
-        // Send packet
-        print_packet(p);
-        window[p.sequence_number % WINDOW_SIZE] = p;
-        if ((p.sequence_number - base) % (MAX_SEQUENCE_NUMBER + 1) == WINDOW_SIZE - 1)
-        {
-            // Window is full, wait for acks
-            while (true)
-            {
-                packet ack;
-                // Receive ack
-                print_packet(ack);
-                if (ack.sequence_number == (base + WINDOW_SIZE - 1) % (MAX_SEQUENCE_NUMBER + 1))
-                {
-                    // Ack is for last packet in window
-                    base = (base + WINDOW_SIZE) % (MAX_SEQUENCE_NUMBER + 1);
-                    
-                    for (int i = 0; i < WINDOW_SIZE; i++)
-                    {
-                        int index = (base + i) % (MAX_SEQUENCE_NUMBER + 1);
-                        print_packet(window[index]);
-                        if (index == next_sequence_number)
-                            break;
-                    }
-                    break;
-                }
-            }
-        }
-    }
-}
-
-void receiver()
-{
-    while (true)
-    { 
-        packet p;
-        // Receive packet
-        if (p.sequence_number == base)
-        {
-            // Packet is expected, send ack
-            print_packet(p);
-            base = (base + 1) % (MAX_SEQUENCE_NUMBER + 1);
-        }
-        else
-        {
-            // Packet is unexpected, ignore
-        }
-    }
-}
-
-void print_packet(const packet &p)
-{
-    cout << "Sequence number: " << p.sequence_number << endl;
-    cout << "Data: " << p.data << endl;
-}
-
-int main()
-{
-    sender();
-    return 0;
-}
+Sending packet with sequence number: 0
+Starting timer.
+Enter received acknowledgment (enter -1 if no ack received): 1
+Received acknowledgment for packet with sequence number: 0
+Sending packet with sequence number: 1
+Enter received acknowledgment (enter -1 if no ack received): 3
+Checking for timeout.
+Timeout occurred for packet with sequence number: 1
+Starting timer.
+Sending packet with sequence number: 1
+Enter received acknowledgment (enter -1 if no ack received): 2
+Received acknowledgment for packet with sequence number: 1
+Sending packet with sequence number: 2
+Enter received acknowledgment (enter -1 if no ack received): -1
+Sending packet with sequence number: 3
+Starting timer.
+Enter received acknowledgment (enter -1 if no ack received): -1
+Checking for timeout.
+Timeout occurred for packet with sequence number: 2
+Timeout occurred for packet with sequence number: 3
+Starting timer.
+Sending packet with sequence number: 2
+Enter received acknowledgment (enter -1 if no ack received): -1
+Checking for timeout.
+Timeout occurred for packet with sequence number: 2
+Timeout occurred for packet with sequence number: 3
+Starting timer.
+Sending packet with sequence number: 2
+Enter received acknowledgment (enter -1 if no ack received): 4
+Received acknowledgment for packet with sequence number: 2
+Sending packet with sequence number: 3
+Enter received acknowledgment (enter -1 if no ack received): 5
+Received acknowledgment for packet with sequence number: 3
+Sending packet with sequence number: 4
+Enter received acknowledgment (enter -1 if no ack received): 6
+Received acknowledgment for packet with sequence number: 4
+All packets sent and acknowledged. Exiting.

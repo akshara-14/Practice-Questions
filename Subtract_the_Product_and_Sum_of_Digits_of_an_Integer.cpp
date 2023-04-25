d52dc3b80e5f27f9a6daf0c6e4cb1869cad3c1c7
Akshara Johari <akshara.johari.14@gmail.com>
2023-04-25 12:33:58 +0530

    LeetCode 1281

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int subtractProductAndSum(int n)
+    {
+        int p = 1;
+        int sum = 0;
+        while (n != 0)
+        {
+            int d = n % 10;
+            sum += d;
+            p *= d;
+            n /= 10;
+        }
+        return p - sum;
+    }
+};

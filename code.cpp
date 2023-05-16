2db34eb43089e08aee4debf204fb34f289c3871e
Akshara Johari <akshara.johari.14@gmail.com>
2023-05-16 16:04:45 +0530

    Interview

--- code.cpp
+++ code.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 int main()
 {
-    int arr[] = {1,2,2,3,4,1};
+    int arr[] = {1, 2, 2, 3, 4, 1};
     vector<int> v(100, -1);
     int n = sizeof(arr) / sizeof(int);
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         v[arr[i]]++;
-        if(v[arr[i]] == true) {
-            cout<<i<<endl;
+        if (v[arr[i]] == true)
+        {
+            cout << i << endl;
             return 0;
         }
     }
-    cout<<"No repeating number found"<<endl;
+    cout << "No repeating number found" << endl;
     return 0;
 }
